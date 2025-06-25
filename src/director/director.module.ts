f281eccfc17adcb06c40b2ee20e836ae1e56b179
pilsung-choi <vlftjd159753@gmail.com>
2025-06-26 01:07:45 +0900

    director migration

--- src/director/director.module.ts
+++ src/director/director.module.ts
@@ -4,9 +4,10 @@ import { DirectorController } from './director.controller';
 import { Director } from './entity/director.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Movie } from 'src/movie/entity/movie.entity';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Director])],
+  imports: [TypeOrmModule.forFeature([Director]), CommonModule],
   controllers: [DirectorController],
   providers: [DirectorService],
 })
--- src/director/director.service.ts
+++ src/director/director.service.ts
@@ -4,68 +4,93 @@ import { UpdateDirectorDto } from './dto/update-director.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Director } from './entity/director.entity';
 import { Repository } from 'typeorm';
+import { PrismaService } from 'src/common/prisma.service';
 
 @Injectable()
 export class DirectorService {
   constructor(
-    @InjectRepository(Director)
-    private readonly directorRepo: Repository<Director>,
+    // @InjectRepository(Director)
+    // private readonly directorRepo: Repository<Director>,
+    private readonly prisma: PrismaService,
   ) {}
   create(createDirectorDto: CreateDirectorDto) {
-    return this.directorRepo.save(createDirectorDto);
+    return this.prisma.director.create({
+      data: createDirectorDto,
+    });
+    //return this.directorRepo.save(createDirectorDto);
   }
 
   findAll() {
-    return this.directorRepo.find();
+    return this.prisma.director.findMany();
+    // return this.directorRepo.find();
   }
 
   findOne(id: number) {
-    return this.directorRepo.findOne({
-      where: {
-        id,
-      },
+    return this.prisma.director.findUnique({
+      where: { id },
     });
+    // return this.directorRepo.findOne({
+    //   where: {
+    //     id,
+    //   },
+    // });
   }
 
   async update(id: number, updateDirectorDto: UpdateDirectorDto) {
-    const director = await this.directorRepo.findOne({
-      where: {
-        id,
-      },
+    const director = await this.prisma.director.findUnique({
+      where: { id },
     });
+    // const director = await this.directorRepo.findOne({
+    //   where: {
+    //     id,
+    //   },
+    // });
 
     if (!director) {
       throw new NotFoundException('존재하지 않는 ID의 영화입니다.');
     }
 
-    await this.directorRepo.update(
-      {
-        id,
-      },
-      { ...updateDirectorDto },
-    );
-
-    const newDirector = await this.directorRepo.findOne({
-      where: {
-        id,
-      },
+    await this.prisma.director.update({
+      where: { id },
+      data: { ...updateDirectorDto },
     });
+    // await this.directorRepo.update(
+    //   {
+    //     id,
+    //   },
+    //   { ...updateDirectorDto },
+    // );
+
+    const newDirector = await this.prisma.director.findUnique({
+      where: { id },
+    });
+    // const newDirector = await this.directorRepo.findOne({
+    //   where: {
+    //     id,
+    //   },
+    // });
 
     return newDirector;
   }
 
   async remove(id: number) {
-    const director = await this.directorRepo.findOne({
-      where: {
-        id,
-      },
+    const director = await this.prisma.director.findUnique({
+      where: { id },
     });
+    // const director = await this.directorRepo.findOne({
+    //   where: {
+    //     id,
+    //   },
+    // });
 
     if (!director) {
       throw new NotFoundException('존재하지 않는 ID의 영화입니다.');
     }
 
-    await this.directorRepo.delete(id);
+    await this.prisma.director.delete({
+      where: { id },
+    });
+    // await this.directorRepo.delete(id);
 
     return id;
   }
